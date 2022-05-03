5990447246188ef5d634aff1e82df3ce939f5f58
thewinger <winger.87@gmail.com>
2022-05-03 18:32:29 +0200

    pckaer-compiled

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -84,11 +84,6 @@ _G.packer_plugins = {
     path = "/Users/win/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
-  ["alpha-nvim"] = {
-    loaded = true,
-    path = "/Users/win/.local/share/nvim/site/pack/packer/start/alpha-nvim",
-    url = "https://github.com/goolord/alpha-nvim"
-  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "/Users/win/.local/share/nvim/site/pack/packer/start/cmp-buffer",
@@ -318,5 +313,6 @@ if should_profile then save_profiles() end
 end)
 
 if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
   vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
 end
