80f4f2abca9b149b41767912658faa032ee89b6f
thewinger <winger.87@gmail.com>
2022-07-15 11:01:54 +0200

    removes plist plugin

--- lua/win/plugins.lua
+++ lua/win/plugins.lua
@@ -114,6 +114,7 @@ return require("packer").startup(function(use)
 	-- Git
 	use({ "lewis6991/gitsigns.nvim" })
 
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
