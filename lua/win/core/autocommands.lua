22510142a7727e0f51f1887e0200067efbb0a880
thewinger <winger.87@gmail.com>
2024-02-23 08:43:23 +0100

    adds vim tmux nav

--- lua/win/core/autocommands.lua
+++ lua/win/core/autocommands.lua
@@ -86,6 +86,16 @@ vim.api.nvim_exec(
   true
 )
 
+-- Autoclose nvim-tree if last window
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd("quit")
+    end
+  end,
+})
+
 -- Show line diagnostics automatically in hover window
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
