ca7402dd3bf2840026d88acad4dc62ebc70a1e64
thewinger <winger.87@gmail.com>
2024-02-28 10:49:08 +0100

    adds nvim-spider

---
+++ lua/plugins/nvim-spider.lua
@@ -0,0 +1,21 @@
+-- lazy.nvim
+return {
+  "chrisgrieser/nvim-spider",
+  keys = {
+    {
+      "w",
+      "<cmd>lua require('spider').motion('w')<CR>",
+      mode = { "n", "o", "x" },
+    },
+    {
+      "e",
+      "<cmd>lua require('spider').motion('e')<CR>",
+      mode = { "n", "o", "x" },
+    },
+    {
+      "b",
+      "<cmd>lua require('spider').motion('b')<CR>",
+      mode = { "n", "o", "x" },
+    },
+  },
+}
