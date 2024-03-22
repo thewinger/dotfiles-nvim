1e028b9b2e9839dddeff1963ce7d693b0016d47e
thewinger <winger.87@gmail.com>
2024-03-22 09:32:36 +0100

    updates

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -50,47 +50,49 @@ local progress = function()
 end
 
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
 
-      options = {
-        globalstatus = true,
-        icons_enabled = true,
-        -- theme = "duskfox",
-        theme = "auto",
-        component_separators = "",
-        section_separators = "",
-        disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-        always_divide_middle = true,
-      },
-      sections = {
-        lualine_a = { mode },
-        lualine_b = { branch },
-        lualine_c = {
-          {
-            "filename",
-            file_status = true,
-            path = 1,
-            shorting_target = 40,
-            symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-          },
+        options = {
+          globalstatus = true,
+          icons_enabled = true,
+          -- theme = "duskfox",
+          theme = "auto",
+          component_separators = "",
+          section_separators = "",
+          disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+          always_divide_middle = true,
         },
-        -- lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_x = { diagnostics, "encoding", filetype },
-        lualine_y = { progress },
-        lualine_z = { location },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = {},
-    })
-  end,
+        sections = {
+          lualine_a = { mode },
+          lualine_b = { branch },
+          lualine_c = {
+            {
+              "filename",
+              file_status = true,
+              path = 1,
+              shorting_target = 40,
+              symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+            },
+          },
+          -- lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_x = { diagnostics, "encoding", filetype },
+          lualine_y = { progress },
+          lualine_z = { location },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        extensions = {},
+      })
+    end,
+  },
 }
