e334a9476c021aec0d1df145d90ca6c26ca58ec0
thewinger <winger.87@gmail.com>
2024-04-29 13:08:18 +0200

    updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,4 @@
 return {
-  -- {
-  --   "thewinger/tailscheme.nvim",
-  --   dev = true,
-  --   lazy = false,
-  --   name = "tailscheme",
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd([[colorscheme tailscheme]])
-  --   end,
-  -- },
   {
     dir = "/Users/win/Projects/tailscheme.nvim",
     dependencies = {
@@ -21,12 +11,4 @@ return {
       vim.cmd([[colorscheme tailscheme]])
     end,
   },
-  -- {
-  --   "EdenEast/nightfox.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     -- vim.cmd.colorscheme("duskfox")
-  --   end,
-  -- },
 }
