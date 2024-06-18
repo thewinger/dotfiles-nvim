230273ff7a877f68eb56f140f9868e88fdcee827
thewinger <winger.87@gmail.com>
2024-06-18 16:11:05 +0200

    Confirm candidate on TAB immediately when there's only one completion entry

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -45,7 +45,14 @@ return {
     local cmp = require("cmp")
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    local luasnip = require("luasnip")
     require("luasnip.loaders.from_vscode").lazy_load()
+
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
 
     cmp.setup({
       completion = {
@@ -63,7 +70,6 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<C-y>"] = cmp.mapping.confirm(
           cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
@@ -71,6 +77,48 @@ return {
           }),
           { "i", "c" }
         ),
+        ["<CR>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({
+                select = true,
+              })
+            end
+          else
+            fallback()
+          end
+        end),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            else
+              cmp.select_next_item()
+            end
+          --[[ Replace with your snippet engine (see above sections on this page)
+      elseif snippy.can_expand_or_advance() then
+        snippy.expand_or_advance() ]]
+          elseif has_words_before() then
+            cmp.complete()
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            end
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       },
       -- sources for autocompletion
       sources = cmp.config.sources({
