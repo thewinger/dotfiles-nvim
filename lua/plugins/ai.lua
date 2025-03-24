33e017aef1d4b486e28b3a99e364da0840e4f946
thewinger <winger.87@gmail.com>
2025-03-24 10:30:11 +0100

    Update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -16,6 +16,7 @@ return {
       --- The below dependencies are optional,
       "saghen/blink.cmp",
       "echasnovski/mini.icons",
+      "folke/snacks.nvim",
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
@@ -75,46 +76,6 @@ return {
           provider = "snacks",
           -- Options override for custom providers
           -- provider_opts = {},
-        },
-        -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
-        system_prompt = function()
-          local hub = require("mcphub").get_hub_instance()
-          return hub:get_active_servers_prompt()
-        end,
-        -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
-        custom_tools = function()
-          return {
-            require("mcphub.extensions.avante").mcp_tool(),
-          }
-        end,
-      })
-    end,
-  },
-  {
-    "ravitemer/mcphub.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
-    },
-    -- cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
-    build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
-    config = function()
-      require("mcphub").setup({
-        -- Required options
-        port = 3050, -- Port for MCP Hub server
-        config = vim.fn.expand("~/.config/nvim/mcpservers.json"), -- Absolute path to config file
-
-        -- Optional options
-        on_ready = function(hub)
-          -- Called when hub is ready
-        end,
-        on_error = function(err)
-          -- Called on errors
-        end,
-        log = {
-          level = vim.log.levels.WARN,
-          to_file = false,
-          file_path = nil,
-          prefix = "MCPHub",
         },
       })
     end,
