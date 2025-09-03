c279a0e7a985941f8ad6eec17604360deb45712f
Alejandro Camara <alejandro.camara@unisys.altia.es>
2025-09-03 08:18:35 +0200

    Update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,26 +6,8 @@ return {
     opts = {},
     config = function()
       -- vim.cmd.colorscheme("base2tone_tailwind_dark")
-      -- vim.cmd.colorscheme("default_tailwind")
+      vim.cmd.colorscheme("default_tailwind")
     end,
   },
   -- Lua
-
-  {
-    "olivercederborg/poimandres.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("poimandres").setup({
-        -- leave this setup function empty for default config
-        -- or refer to the configuration section
-        -- for configuration options
-      })
-    end,
-
-    -- optionally set the colorscheme within lazy config
-    init = function()
-      vim.cmd("colorscheme poimandres")
-    end,
-  },
 }
